ab08b2f57b2d49f39e245d6261187fb519505eb0
emkis <nicolasemkis@gmail.com>
2024-02-11 18:08:22 +0000

    add entry point for native

---
+++ packages/payments/index.native.ts
@@ -0,0 +1,7 @@
+/**
+ * This file is the entry point for the payments package, which by default
+ * will export web specific code and everything that is shared and cross-platform.
+ */
+
+export * from "./shared";
+export * from "./native";
--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -1,6 +1,6 @@
 /**
- * This file is the default entry point for the payments package, which by
- * default targets the web platform.
+ * This file is the alternative entry point for the payments package, which by
+ * targets the native platform.
  *
  * We also exports everything else from the `shared` folder, as they are platform
  * agnostic and can be used in both web and native.
